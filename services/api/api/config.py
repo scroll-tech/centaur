5d3b6a15c1cbf90cccfca7d427c6cfe1893f2762
zhygis <5236121+Zygimantass@users.noreply.github.com>
2026-05-28 20:44:19 +0200

    Support CORS origin regex (#274)

--- services/api/api/config.py
+++ services/api/api/config.py
@@ -9,6 +9,13 @@ class Settings(BaseSettings):
 
     database_url: str = Field(alias="DATABASE_URL")
     cors_origins: list[str] = []
+    cors_origin_regex: str | None = None
+
+    @property
+    def cors_allow_origin_regex(self) -> str | None:
+        if self.cors_origin_regex == "*":
+            return ".*"
+        return self.cors_origin_regex
 
 
 settings = Settings()
